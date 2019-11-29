13972d580fe0fb1bc8bb610e04b1c9dd1c66649b
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2019-11-29 16:29:21 +0300

    fix elem

--- src/components/Main/Content/index.js
+++ src/components/Main/Content/index.js
@@ -2,16 +2,64 @@ import React, { useState, useEffect } from 'react'
 import './style.css'
 
 function Content() {
-  const [count, setCount] = useState(0)
+  const helloText = 'привет, как дела?';
+  const rndNumArr = [1,2,3,4,5,6].map(()=>Math.floor(Math.random()*100));
+
+  const [isShowForm, setShowForm] = useState(false);
+  const [isShowTable, setShowTable] = useState(true);
+  
+  const formItemList = rndNumArr.map((item, index)=> 
+    <p key={index}>{item}</p>
+  );
+  const contentForm = isShowForm && (
+    <form id='formContent' >
+      {formItemList}
+    </form>
+  );
+
+  const contentTable = isShowTable && (
+    <>
+      <table id='tableContent' className='collapsed hiddEmpCells' >
+        <caption class='captBott' >Новинки</caption>
+        <thead>
+          <th>index</th>
+          <th>value</th>
+        </thead>
+        <tbody>
+          { 
+            rndNumArr.map((item, index)=>
+              <tr key={index} >
+                <td>
+                  {index}
+                </td>
+                <td>
+                  
+                </td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+    </>
+  );
+
   useEffect(() => {
-    document.title = `${count} times`
-  }, [count])
+    document.title = `${isShowForm}`
+  }, [isShowForm] );
+
   return (
-    <div className="content">
-      <button onClick={() => setCount(count + 1)}>Click</button>
-      <div>{count}</div>
+    <div className='content' >
+      <span id='headInContent' className='block' >{helloText}</span>
+      <button id='buttForm' className='pointer block' onClick={() => setShowForm(!isShowForm)}>
+        { !isShowForm ? 'Show' : 'Hide'}
+      </button>
+      {contentForm}
+      <button id='buttForm' className='pointer block' onClick={() => setShowTable(!isShowTable)}>
+        { !isShowTable ? 'Show' : 'Hide'}
+      </button>
+      {contentTable}
     </div>
   )
 }
 
-export default Content
+export default Content;
