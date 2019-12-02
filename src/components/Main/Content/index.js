874205b74e90f50e6f651817c130630c9eab5a55
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2019-12-02 16:50:41 +0300

    ref css

--- src/components/Main/Content/index.js
+++ src/components/Main/Content/index.js
@@ -1,64 +1,59 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState} from 'react'
 import './style.css'
 
 function Content() {
-  const helloText = 'привет, как дела?';
-  const rndNumArr = [1,2,3,4,5,6].map(()=>Math.floor(Math.random()*100));
-
-  const [isShowForm, setShowForm] = useState(false);
-  const [isShowTable, setShowTable] = useState(true);
-  
-  const formItemList = rndNumArr.map((item, index)=> 
-    <p key={index}>{item}</p>
-  );
-  const contentForm = isShowForm && (
-    <form id='formContent' >
-      {formItemList}
-    </form>
-  );
-
-  const contentTable = isShowTable && (
-    <>
-      <table id='tableContent' className='collapsed hiddEmpCells' >
-        <caption class='captBott' >Новинки</caption>
-        <thead>
-          <th>index</th>
-          <th>value</th>
-        </thead>
-        <tbody>
-          { 
-            rndNumArr.map((item, index)=>
-              <tr key={index} >
-                <td>
-                  {index}
-                </td>
-                <td>
-                  
-                </td>
-              </tr>
-            )
-          }
-        </tbody>
-      </table>
-    </>
-  );
-
-  useEffect(() => {
-    document.title = `${isShowForm}`
-  }, [isShowForm] );
-
+  //const [menuItem, setMenuItem] = useState(1);
+  const menuArr = ['Main', 'Blog', 'Forum', 'Contacts', 'About site'].map((item, index)=>(
+    <li key={index}>
+      <a href='/#' >{item}</a>
+    </li>
+  ));
   return (
-    <div className='content' >
-      <span id='headInContent' className='block' >{helloText}</span>
-      <button id='buttForm' className='pointer block' onClick={() => setShowForm(!isShowForm)}>
-        { !isShowForm ? 'Show' : 'Hide'}
-      </button>
-      {contentForm}
-      <button id='buttForm' className='pointer block' onClick={() => setShowTable(!isShowTable)}>
-        { !isShowTable ? 'Show' : 'Hide'}
-      </button>
-      {contentTable}
-    </div>
+    <>
+      <div id='header' >
+        <h1>MySite</h1>
+        <div id='nav'>
+          <ul>
+            {menuArr}
+          </ul>
+        </div>
+      </div>
+      <div id='wrapper' >
+        <div id='sidebar1' className='aside' >
+          <h2>The standart Lorem Ipsum </h2>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua..."
+          </p>
+        </div>
+        <div id='sidebar2' className='aside' >
+          <h2>1914 translation by H. Rackham</h2>
+          <p>
+            It is a long established fact that a reader will be distracted by the readable 
+            content of a page when looking at its layout.
+          </p>
+          <h3>Options</h3>
+          <ul>
+            <li>Item1</li>
+            <li>Item2</li>
+            <li>Item3</li>
+          </ul>
+        </div>
+        <div id='article'>
+            <h2>What is Lorem Ipsum?</h2>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+            <p>
+              Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of 
+              classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, 
+              a Latin professor at Hampden-Sydney College in Virginia...
+            </p>
+          </div>
+      </div>
+      <div id='footer'>
+        <p>Contacts: admin@mysyte.com</p>
+        <p>Copyright © MySyte.com, 2016</p>
+      </div>
+    </>
   )
 }
 
