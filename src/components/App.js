14110b2e0de585d34ef12b6972cde9336254fd95
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2019-12-02 16:50:06 +0300

    up main js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Content from '../components/Main/Content/index.js'
 import Footer from '../components/Main/Footer/index.js'
 import Header from '../components/Main/Header/index.js'
 
 function App() {
+  const [isExist, setExist] = useState(false);
   const footObj = {
     date: new Date().toLocaleTimeString(),
     name: 'rPetProj',
   }
   return (
     <>
-      <Header />
+      { isExist && <Header /> }
       <Content />
-      <Footer footerObj={footObj} />
+      { isExist && <Footer footerObj={footObj} /> }
     </>
   )
 }
