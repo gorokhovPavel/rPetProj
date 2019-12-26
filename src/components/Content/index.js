c5d649e19ad310dff9054ec03ff6aa050b8f30de
gorokhovPavel <pgorokhov@yandex.ru>
2019-12-26 03:19:24 +0300

    ref after merging

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -4,14 +4,16 @@ import CardList from '../CardList/index'
 import AddCard from '../AddCard/index'
 import Filter from '../Filter/index'
 
+import './style.css'
+
 function Content() {
-    return (
-        <>
-            <AddCard />
-            <Filter />
-            <CardList />
-        </>
-    )
+  return (
+    <>
+      <AddCard />
+      <Filter />
+      <CardList />
+    </>
+  )
 }
 
-export default Content;
+export default Content
