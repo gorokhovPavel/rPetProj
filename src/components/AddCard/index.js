d10373313554de751ae71c399ec86e0999a022de
gorokhovPavel <pgorokhov@yandex.ru>
2019-12-26 03:58:59 +0300

    add autocomplete

--- src/components/AddCard/index.js
+++ src/components/AddCard/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { AutoComplete } from 'antd'
+import { allCitiesSelector, citiesLoading } from '../../store/selectors'
 
-function AddForm(){
-    return (
-        <div className='inlineBox' >
-            <input type='text' />
-            +
-        </div>
-    )
+function AddForm({ allCities, loading }) {
+  const allNamesOfCities =
+    allCities.length > 0 ? allCities.map(item => item.name) : []
+  const InputLoading = !loading && (
+    <AutoComplete dataSource={allNamesOfCities} />
+  )
+  return <div className="inlineBox">{InputLoading}+</div>
 }
-
-export default AddForm;
+export default connect(state => ({
+  allCities: allCitiesSelector(state),
+  loading: citiesLoading(state),
+}))(AddForm)
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,19 +1,20 @@
 import { createSelector } from 'reselect'
 
-export const citiesLoading = state => state.cities.loading || state.cities.loading;
+const filtersSelector = state => state.filters
+const citiesSelector = state => state.cities.entities
 
-const filtersSelector = state => state.filters;
-const citiesSelector = state => state.cities.entities;
+export const citiesLoading = state =>
+  state.cities.loading || state.cities.loading
 
-export const filtratedCitiesSelector = createSelector( citiesSelector, filtersSelector, ( cities, filters ) =>
+export const allCitiesSelector = createSelector(citiesSelector, cities =>
+  cities.valueSeq().toArray()
+)
+export const filtratedCitiesSelector = createSelector(
+  citiesSelector,
+  filtersSelector,
+  (cities, filters) =>
     cities
-        .valueSeq()
-        .toArray()
-        .filter( item => item.degree >= filters.degreeFilter )
-);
-
-// export const avarageRateSelector = ( state, {city} ) =>
-//     city.degree
-//         .map( review => review && review.rating )
-//         .filter( rate => typeof rate !== 'undefined')
-//         .reduce( (acc, el, _, arr) => acc + el / arr.length, 0 );
+      .valueSeq()
+      .toArray()
+      .filter(item => item.degree >= filters.degreeFilter)
+)
